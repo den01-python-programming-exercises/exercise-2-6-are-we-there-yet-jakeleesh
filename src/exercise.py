3c35c9d8e56e459730d2eeed0f2514e426a8e562
Jake Lee <jake.lee@fdmgroup.com>
2021-09-02 08:14:43 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #write your code below this line
+    while True:
+        num = int(input("Give a number:"))
+        if (num == 4):
+            break
 
 if __name__ == '__main__':
     main()
